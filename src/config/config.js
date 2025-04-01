916be1ac1566955239079b64f212f7162902fe50
Tedgey <tom.edgeworth47@gmail.com>
2025-04-01 14:06:27 -0400

    Implement postgres with local database. Ran first migration.

---
+++ src/config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config() // Load environment variables from .env
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_DATABASE_DEV || 'database_development',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 5432,
+    dialect: process.env.DB_DIALECT || 'postgres',
+  },
+  test: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_DATABASE_TEST || 'database_test',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: process.env.DB_DIALECT || 'postgres',
+  },
+  production: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_DATABASE_PROD || 'database_production',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: process.env.DB_DIALECT || 'postgres',
+  },
+}
